e9ad8087f0f0c100ee061f4f3db0d3310a4176e6
billy-yuan <billy.yuan1@gmail.com>
2021-07-16 13:05:36 -0400

    remove console log in selectText.js

--- app/selectText.js
+++ app/selectText.js
@@ -1,9 +1,5 @@
 document.addEventListener("keydown", (e) => {
   const selectedText = window.getSelection().toString();
 
-  console.log(selectedText);
-
-  chrome.runtime.sendMessage(selectedText, (r) => {
-    console.log(r);
-  });
+  chrome.runtime.sendMessage(selectedText);
 });
